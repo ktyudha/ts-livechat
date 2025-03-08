df0c262de43eed00ada13f2fe8731bc2332d8f45
ktyudha <ktyudha@gmail.com>
2025-03-08 15:21:27 +0700

    update stringify

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -3,7 +3,16 @@ const router = express.Router();
 
 router.post("/", (req, res) => {
   const data = req.body;
-  console.log(" Received webhook", data);
+  console.log("Received webhook:", JSON.stringify(data, null, 2));
+
+  // if (data.uplink_message && data.uplink_message.rx_metadata) {
+  //   console.log(
+  //     "📡 rx_metadata:",
+  //     JSON.stringify(data.uplink_message.rx_metadata, null, 2)
+  //   );
+  // } else {
+  //   console.log("⚠️ No rx_metadata found in the request");
+  // }
 
   res.status(200).send("Webhook received");
 });
